223bc86fdaee0245598fdd60ff901e028f359f07
Anastasia <barabanova.aaa@gmail.com>
2023-10-19 23:54:18 +0300

    Change code

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int amount = 400;
+        int amount = 100;
 
-        int refill = 800;
+        int refill = 1100;
 
         int finalScore;
         finalScore = amount + refill;
 
         System.out.println("Количество бонусных рублей:");
-        if (finalScore > 1000) {
-            System.out.println(finalScore / 100);
+        if (refill > 1000) {
+            System.out.println(refill / 100);
         }
 
         int bonuses = finalScore / 100;

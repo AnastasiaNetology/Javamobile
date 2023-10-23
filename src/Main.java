f8a16d248011e004240cceb5935b324fde77dc46
Anastasia <barabanova.aaa@gmail.com>
2023-10-23 15:06:29 +0300

    Add variable

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int amount = 100;
-
-        int refill = 1100;
-
+        int amount = 100;     //счет
+        int refill = 1100;    //сумма пополнения
+        int bonusCoef = 100;  //бонусный коэффициент
         int finalScore;
         finalScore = amount + refill;
 
         System.out.println("Количество бонусных рублей:");
-        if (refill > 1000) {
-            System.out.println(refill / 100);
+        if (refill >= 1000) {
+            System.out.println(refill / bonusCoef);
         }
         if (refill < 1000) {
             System.out.println(refill = 0);
         }
 
-        int bonuses = refill / 100;
+        int bonuses = refill / bonusCoef;
 
         System.out.println("Итоговый счет:");
         System.out.println(finalScore + bonuses);

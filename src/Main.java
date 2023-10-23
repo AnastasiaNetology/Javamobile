b53c8aa9540422f7ee4370ea99e128ebbc6a322c
Anastasia <barabanova.aaa@gmail.com>
2023-10-23 22:19:56 +0300

    Finishto

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
-
     public static void main(String[] args) {
 
-        int amount = 300;     //счет
-        int refill = 1500;    //сумма пополнения
-        int bonusCoef = 100;  //бонусный коэффициент
-        int finalScore;
-        finalScore = amount + refill;
+        int amount = 300;                   //текущий счет абонента до пополнения
+        int refill = 1500;                  //сумма пополнения
+        int bonusCoef = 100;                //бонусный расчет
+        int finalScore = amount + refill;   //итоговый счет (текущий счет + сумма пополнения)
+        int bonuses = refill / bonusCoef;   //итоговый бонус
+
+        if (refill >= 1000){
+            bonuses = refill / bonusCoef;
+        } else {
+            bonuses = 0;
+        }
 
         System.out.println("Количество бонусных рублей:");
-        if (refill >= 1000) {
-            System.out.println(refill / bonusCoef);
-        }
-        if (refill < 1000) {
-            System.out.println(refill = 0);
-        }
-
-        int bonuses = refill / bonusCoef;
-
+        System.out.println("" + bonuses);
         System.out.println("Итоговый счет:");
         System.out.println(finalScore + bonuses);
     }
